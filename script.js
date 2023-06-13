c9e8fe71f80ab33d56359a0ad99a0ea4ec0e86b5
DKXLY <77124386+DKXLY@users.noreply.github.com>
2023-06-13 23:07:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const btn = document.querySelector('btn');
-const tabs = document.querySelectorAll('tab');
 
-btn.addEventListener('click', () => {
-  tabs.forEach(tab => {
-    tab.style.display = 'block';
-    tab.style.animation = 'slideIn 0.5s ease-in-out forwards';
-  });
-});
+
+function animateTabs() {
+  var tabs = document.getElementsByClassName('tab');
+  var delay = 100;
+
+  for (var i = 0; i < tabs.length; i++) {
+    (function(index) {
+      setTimeout(function() {
+        tabs[index].style.display = 'block';
+        tabs[index].style.opacity = '0';
+        tabs[index].style.transform = 'translateY(20px)';
+        tabs[index].style.transition = 'opacity 0.5s, transform 0.5s';
+        setTimeout(function() {
+          tabs[index].style.opacity = '1';
+          tabs[index].style.transform = 'translateY(0)';
+        }, 50);
+      }, delay);
+    })(i);
+
+    delay += 100;
+  }
+}
+
+
+
