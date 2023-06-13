2918c809c6e6f20a395b25c581b29d3d29431cdf
DKXLY <77124386+DKXLY@users.noreply.github.com>
2023-06-13 20:47:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-window.addEventListener('scroll', function() {
-  var tabsContainer = document.querySelector('.tabs-container');
-  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-
-  if (scrollTop > 100 || tabsContainer.classList.contains('open')) {
-    tabsContainer.classList.add('visible');
-  } else {
-    tabsContainer.classList.remove('visible');
-  }
-});
-
 var viewModsButton = document.querySelector('.btn');
 var tabsContainer = document.querySelector('.tabs-container');
+var isOpen = false;
 
 viewModsButton.addEventListener('click', function(event) {
   event.preventDefault();
-  tabsContainer.classList.toggle('open');
+  isOpen = !isOpen;
+  tabsContainer.classList.toggle('open', isOpen);
 });
+
+window.addEventListener('scroll', function() {
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  var shouldShowTabs = scrollTop > 100 || isOpen;
+  tabsContainer.classList.toggle('visible', shouldShowTabs);
+});
+
