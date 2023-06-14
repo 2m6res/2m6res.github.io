fa3055d8b56856ce19ed8f844ae5e4727841899f
DKXLY <77124386+DKXLY@users.noreply.github.com>
2023-06-14 15:28:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
         $(document).ready(function() {
 
             $("a").sparkle({ 
-
-                "color": ["#ff0080","#ff0080","#FFF"] , 
                 "minSize": 2 , 
                 "maxSize": 10 ,
                 "overlap": 20 ,
