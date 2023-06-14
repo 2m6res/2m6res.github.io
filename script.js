b156576a6e6bb19ea34c5406700f9efe157ea430
DKXLY <77124386+DKXLY@users.noreply.github.com>
2023-06-14 15:24:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,10 @@
             $("a").sparkle({ 
 
                 "color": ["#ff0080","#ff0080","#FFF"] , 
-                count: 70 ,
-                speed: 0
-
-            });
-
-            $("modrinthC").sparkle({
-
-                color: ["#2eafea","#e56604"],
-                speed: 0.4
-
-            });
-          
-          
-            $("curseforgeC").sparkle({
-
-                color: ["#2eafea","#e56604"],
-                speed: 0.4
-
+                "minSize": 2 , 
+                "maxSize": 10 ,
+                "overlap": 20 ,
+                "direction": "up" ,
+                "speed": 0.4
             });
         });
