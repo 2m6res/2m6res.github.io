32f49fe8bd7efed3206df26a61c25db505c81b92
DKXLY <77124386+DKXLY@users.noreply.github.com>
2023-06-14 17:30:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
      $("img").sparkle({ 
-             "minSize": 2 , 
-             "maxSize": 5 ,
+             "minSize": 10 , 
+             "maxSize": 12 ,
              "overlap": 0 ,
              "direction": "up" ,
-             "speed": 0.4
+             "speed": 3
       });
 });
