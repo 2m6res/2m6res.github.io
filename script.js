544e68064da3d308686304d484ee61f2b2246cd8
DKXLY <77124386+DKXLY@users.noreply.github.com>
2023-06-14 15:41:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
             $("img").sparkle({ 
                 "minSize": 2 , 
                 "maxSize": 5 ,
-                "overlap": 100 ,
+                "overlap": 0 ,
                 "direction": "up" ,
                 "speed": 0.4
             });
