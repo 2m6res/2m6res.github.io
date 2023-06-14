69d692a954cb2b29764fc2c69ea12f85be9a1c40
DKXLY <77124386+DKXLY@users.noreply.github.com>
2023-06-14 15:52:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-        $(document).ready(function() {
-
-            $("img").sparkle({ 
-                "minSize": 2 , 
-                "maxSize": 5 ,
-                "overlap": 0 ,
-                "direction": "up" ,
-                "speed": 0.4
-            });
-        });
-
-        $(document).ready(function() {
-                $('.popup').addClass('show');
-
-                // Close the popup when clicked outside
-                $(document).on('click', function(event) {
-                        if (!$(event.target).closest('.popup').length) {
-                                $('.popup').removeClass('show');
-                        }
-                });
-        });
+$(document).ready(function() {
+     $("img").sparkle({ 
+             "minSize": 2 , 
+             "maxSize": 5 ,
+             "overlap": 0 ,
+             "direction": "up" ,
+             "speed": 0.4
+      });
+});
