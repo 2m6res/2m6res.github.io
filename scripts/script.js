fcf33cfd8769fcd9b48b4a15e28ff8f00518147e
DKXLY <77124386+DKXLY@users.noreply.github.com>
2023-10-19 21:46:33 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 $('img').on('dragstart', function(event) { event.preventDefault(); });
+
+$(document).scroll((e) => {
+    let percentScrolled = window.scrollY / window.innerHeight;
+    
+    container = document.getElementById("container");
+    
+    if(percentScrolled > 0.1 && !container.classList.contains("containerScaleDown")) {
+        if (container.classList.contains("containerScaleUp")) {
+            container.classList.remove("containerScaleUp");
+        }
+        container.classList.add("containerScaleDown");
+    }
+    
+    if (percentScrolled < 0.1 && container.classList.contains("containerScaleDown")) {
+        container.classList.remove("containerScaleDown");
+        container.classList.add("containerScaleUp");
+    }
+});
