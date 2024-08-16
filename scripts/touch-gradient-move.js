0ccd645ac2c8e5a1d3a7344ecb927917993be60d
DKXLY <77124386+DKXLY@users.noreply.github.com>
2024-08-16 16:10:43 +0200

    idk

--- scripts/touch-gradient-move.js
+++ scripts/touch-gradient-move.js
@@ -8,11 +8,6 @@ document.body.ontouchmove = event => {
         "--x": `${clientX}px`,
         "--y": `${clientY}px`
     }, {duration: 0, fill: "forwards"})
-
-    /*
-    light_it_up.animate({
-        mask: `radial-gradient(13rem at ${clientX}px ${clientY}px, rgba(0,0,0,0.8858996479451156) 0%, rgba(0,0,0,0.8606895639115021) 9%, rgba(0,0,0,0.7710537095697654) 26%, rgba(0,0,0,0.5973842417826505) 44%, rgba(0,0,0,0.35690805130646013) 63%, rgba(0,0,0,0.07357471797312676) 91%, rgba(0,0,0,0) 100%)`
-    }, {duration: 0, fill: "forwards"})*/
 }
 
 document.body.ontouchstart = event => {
