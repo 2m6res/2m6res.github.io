f37b4115172a5c0aa0af0d06d2903c031fd706e7
DKXLY <77124386+DKXLY@users.noreply.github.com>
2024-08-16 22:22:04 +0200

    idk

--- scripts/scrolltrigger.js
+++ scripts/scrolltrigger.js
@@ -7,3 +7,19 @@ function raf(time) {
 
 requestAnimationFrame(raf);
 
+gsap.registerPlugin(ScrollTrigger) 
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.container',
+        start: 'top',
+        end: 'bottom',
+        scrub: true,
+        markers: false
+    }
+})
+
+tl.to('.profile-container', {
+    transform: 'translate(-50%, 0) scale(80%)',
+    filter: 'blur(7px)'
+})
